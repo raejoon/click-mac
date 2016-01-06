beece5e4edce677f5ba358530fe7ebc6a0374dcf
Raejoon Jung <raejoon@stanford.edu>
2016-01-05 23:32:44 -0800

    beaconsource sends srcaddr, broadcastaddr, beacon interval

--- fakebeaconsource.cc
+++ fakebeaconsource.cc
@@ -1,6 +1,52 @@
 #include <click/config.h>
+#include <click/args.hh>
 #include "fakebeaconsource.hh"
 CLICK_DECLS
+
+static uint16_t broadcastaddr = 99;
+FakeBeaconSource::FakeBeaconSource() 
+  : _timer(this)
+{
+}
+
+FakeBeaconSource::~FakeBeaconSource()
+{
+  _beacon -> kill();
+}
+
+int FakeBeaconSource::configure(Vector<String> &conf, ErrorHandler* errh)
+{
+  if (Args(conf, this, errh)
+      .read_mp("ADDR", _srcaddr)
+      .read_or_set("INTERVAL", _interval, 100)
+      .complete() < 0)
+      return -1;
+
+  size_t length = sizeof(uint16_t) + sizeof(uint16_t) + sizeof(uint16_t);
+  char payload[length];
+  
+  memcpy(payload, &_srcaddr, sizeof(uint16_t));
+  memcpy(payload + sizeof(uint16_t), &broadcastaddr, sizeof(uint16_t));
+  memcpy(payload + 2*sizeof(uint16_t), &_interval, sizeof(uint16_t));
+
+  _beacon = Packet::make(payload, length);
+  return 0;
+}
+
+int FakeBeaconSource::initialize(ErrorHandler *) {
+  _timer.initialize(this);
+  _timer.schedule_now();
+  return 0;
+}
+
+void FakeBeaconSource::run_timer(Timer *timer) {
+  assert(timer == &_timer);
+
+  Packet* p = _beacon -> clone();
+  
+  output(0).push(p);
+  _timer.reschedule_after_msec(_interval);
+}
 
 CLICK_ENDDECLS
 EXPORT_ELEMENT(FakeBeaconSource)
--- fakebeaconsource.hh
+++ fakebeaconsource.hh
@@ -1,6 +1,7 @@
 #ifndef FAKEBEACONSOURCE_HH
 #define FAKEBEACONSOURCE_HH
 #include <click/element.hh>
+#include <click/timer.hh>
 CLICK_DECLS
 
 class FakeBeaconSource : public Element 
@@ -12,6 +13,16 @@ class FakeBeaconSource : public Element
     const char *class_name() const { return "FakeBeaconSource"; }
     const char *port_count() const { return "0/1"; }
     const char *processing() const { return "PUSH"; }
+    int configure(Vector<String> &conf, ErrorHandler* errh);
+    int initialize(ErrorHandler *);
+    void run_timer(Timer *timer);
+  
+  private:
+    Timer _timer;
+    uint16_t _srcaddr;
+    uint16_t _interval;
+
+    Packet* _beacon;
 };
 
 CLICK_ENDDECLS
